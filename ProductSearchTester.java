4020d5eef5e1b207c013b7d0333eeefc7b7bbed5
Kathleen-Kgomotso <202300412@ub.ac.bw>
2025-04-30 17:24:23 +0200

    Updated ProductSearchTester.java

--- ProductSearchTester.java
+++ ProductSearchTester.java
@@ -2,19 +2,27 @@ public class ProductSearchTester{
     public static void main(String[] args){
         //creation of test products
         Product[] products = {
-            new product(101),
-            new product(102),
-            new product(103)
+            new Product("Carrot", 1.99, 101),
+            new Product("Potato", 1.88, 102),
+            new Product("Cabbage", 2.35, 103)
         };
 
-        //searching for an existing productID
-        int existingId = 103;
-        Product foundProduct = ProductSearch.linearSearchByProductID(products, existingId);
-        if (foundProduct != null && foundProduct.getProductID() == existingId){
-            System.out.println("Product found");
-        }
-        else{
-            System.out.println("Product does not exist");
-        }
+        //test linear search
+        testSearch(products, 102);
+        testSearch(products, 777);
     }
+    
+        private static void testSearch(Product[] products, int targetID){
+            System.out.println("\nSearching for product ID: " + targetID);
+            Product result = ProductSearch.linearSearchByProductID(products, targetID);
+
+            if(result != null) {
+                System.out.println("Found prouct: " + result.getProductName()
+                + ", ID: " + result.getProductID() + ", Price: $" + result.getPrice() + ")");
+            }
+            else{
+                System.out.println("Product with ID " + targetID + " not found");
+            }
+        }
+    
 }
